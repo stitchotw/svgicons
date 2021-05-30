f03554eb14f8f7504cdc23449f2f50590a1ad280
stitchotw <stitchotw@gmail.com>
2021-05-30 23:24:22 +0200

    Begun work on atributes in UI

--- script/attributes.mjs
+++ script/attributes.mjs
@@ -2,9 +2,9 @@
  * 
  */
 
-import {deleteCurrentlySelectedShape} from './workarea.mjs';
-import {addClass, removeClass} from './dom.mjs';
-import {shapeFromId} from './icon.mjs';
+import { deleteCurrentlySelectedShape } from './workarea.mjs';
+import { addClass, removeClass } from './dom.mjs';
+import { shapeFromId } from './icon.mjs';
 
 export function setUpAttributes() {
     addEventListeners();
@@ -12,13 +12,23 @@ export function setUpAttributes() {
 
 function addEventListeners() {
     document.getElementById("delete-selected-shape-button").addEventListener("click", deleteCurrentlySelectedShape);
+
+    const buttons = document.getElementsByClassName("attribute-button");
+
+    for (const button of buttons) {
+        button.addEventListener("click", (evt) => {
+            changeAttribute(evt.target.dataset.attributeName, evt.target.dataset.operation);
+        });
+    }
 }
 
 
 export function selectedShapeChanged(id) {
     hideAllShapeAttributes();
-    if (id)
+    if (id){
         showShapeAttributes(id);
+        updataShapeAttributesView(id);
+    }
 }
 
 function hideAllShapeAttributes() {
@@ -36,5 +46,14 @@ function showShapeAttributes(id) {
     removeClass(document.getElementsByClassName("shape-functions"), "hidden");
 }
 
+function updataShapeAttributesView(id){
+
+}
+
+function changeAttribute(attributeName, operation) {
+    console.log(attributeName + " " + operation);
+
+    updataShapeAttributesView()
+}
 
 
