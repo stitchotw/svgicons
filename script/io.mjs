fce211b9a447ac0bd1bb2641e58a98b59eb236ee
stitchotw <stitchotw@gmail.com>
2021-06-14 14:35:08 +0200

    Added methods to clear icons from local storage

--- script/io.mjs
+++ script/io.mjs
@@ -4,13 +4,29 @@ export const CURRENT_ICON_ID = "_CURRENT_ICON_";
 
 export function saveIconToLocalStorage(id){
     const s = window.localStorage;
-    console.log(s);
-    s.setItem(id, "set");
+    s.setItem(id, JSON.stringify(null));
 }
 
 export function loadIconFromLocalStorage(id){
     const s = window.localStorage;
-    console.log(s);
     const value = s.getItem(id);
-    console.log(s, value);
+
+    if(value){
+        try {
+            const data = JSON.parse(value);
+            console.log(data);
+        } catch (error) {
+            clearIconInLocalStorage(id);
+            alert("Error when loading icon from local storage: "+error+"\nData cleared");            
+        }
+    }
+}
+
+export function clearIconsInLocalStorage(){
+    clearIconInLocalStorage(CURRENT_ICON_ID);
+}
+
+export function clearIconInLocalStorage(id){
+    const s = window.localStorage;
+    s.removeItem(id);
 }
