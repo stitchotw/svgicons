e66b5b15069e9ece8ae98add82d3b31d8f31cd45
stitchotw <stitchotw@gmail.com>
2021-06-21 22:54:10 +0200

    Made a better example for path (#64)

--- script/dialogs.mjs
+++ script/dialogs.mjs
@@ -274,8 +274,8 @@ export function openPolygonDataDialog(data, listener) {
 export function openPathDataDialog(data, listener) {
     const space = /[\s]+/;
     const initialpos = /[Mm]\s+\d+[\s,]+\d+/;
-    const oneparam = /[HhVv]\s+\d+/;
-    const twoparam = /[MmLlTt]\s+\d+[\s,]+\d+/;
+    const oneparam = /[HhVv]\s+-?\d+/;
+    const twoparam = /[MmLlTt]\s+-?\d+[\s,]+-?\d+/;
     const closepath = /[Zz]/;
     const twoparamcurve = /[Cc]\s+\d+\s+\d+[\s,]+\d+\s+\d+/;
     const threeparamcurve = /[Cc]\s+\d+\s+\d+([\s,]+\d+\s+\d+){2}/;
@@ -288,7 +288,7 @@ export function openPathDataDialog(data, listener) {
     const all = "^" + initialpos.source + "(" + space.source + or(oneparam, twoparam, closepath, twoparamcurve, threeparamcurve, arc) + ")+$";
 
     inputDialog.open("Path data",
-        "It is easier to read the data if you put one command per line. Don't put any commas between commands. <a href='https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths' target='_blank' class='help-link' title='Opens in new tab/window'>developer.mozilla.org have a good introduction to path data.</a>",
+        "It is easier to read the data if you put one command per line.<br>Don't put any commas between commands.<br><a href='https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths' target='_blank' class='help-link' title='Opens in new tab/window'>developer.mozilla.org have a good introduction to path data.</a><br>Example: M 5,5 l 11,0 l 0,11 l 11,0 l 0,11 l -22,0 Z",
         data, new RegExp(all), listener);
     return inputDialog.text;
 }
